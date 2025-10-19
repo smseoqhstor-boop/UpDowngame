2c3dee28485d5377bccd8eaa4a53e4107519731c
smseoqhstor-boop <smseoqhstor@gmail.com>
2025-10-19 17:49:01 +0900

    Add files via upload

--- updowngame_test.py
+++ updowngame_test.py
@@ -1,5 +1,6 @@
-com_num = 77
-life = 5
+import random as r
+com_num = r.randrange(1,101)
+user_coin = 100
 
 def updownGame(com_num,user_num):
     if(user_num > com_num):
@@ -9,20 +10,35 @@ def updownGame(com_num,user_num):
     else:
         return True
 
-while(life > 0):
-    print("현재 life : {}".format(life))
+while(user_coin >0):
+    print("현재 coin : {}".format(user_coin))
+    bet_coin = int(input("배팅할 금액: "))
+    print("현재 배팅된 총 coin: ")
 
-    try:
-        user_num = int(input("숫자입력: "))
+    while(bet_coin > user_coin):
+        print("당신이 가지고 있는 코인은 {} 입니다. 다시 입력해라잇!".format(user_coin))
+        bet_coin = int(input("배팅할 금액: "))
 
-        if updownGame(com_num,user_num):
-            print("정답 맞추셨습니다.")
-            exit()
+    life = 10
+    while(life >0):
+        print("현재 남은 기회 : {}".format(life))
+        try:
+            user_num = int(input("숫자입력: "))
 
-        life = life - 1
+            if updownGame(com_num,user_num):
+                print("정답 맞추셨습니다.")
+                break
 
-    except ValueError:
-        print("제대로 된 숫자를 입력해주세요!")
+            life -= 1
+
+        except ValueError:
+            print("제대로 된 숫자를 입력해주세요!")
     
+    if(life > 0):
+        #게임 승리
+        user_coin = user_coin + bet_coin
+    else:
+        #게임 패배
+        user_coin = user_coin - bet_coin
 
-print("기회를 다 잃으셨습니다.ㅠㅠ")
+print("코인을 다 잃으셨습니다.ㅠㅠ")
